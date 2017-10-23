893c4e0de3013dccea6545be0cd8591f644a4156
Tobias David Scheiwiler <tobias.scheiwiler@unisg.ch>
2017-10-23 17:20:58 +0200

    chat-bar

---
+++ src/app/chat-bar/chat-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-bar',
+  templateUrl: './chat-bar.component.html',
+  styleUrls: ['./chat-bar.component.css']
+})
+export class ChatBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
