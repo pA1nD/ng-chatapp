8ae6d85a2c738638e267b8beb8e8a86cf5e9e593
Björn Schmidtke <bs@pa1nd.de>
2017-11-13 16:15:55 +0100

    Make input work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
+import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { Content1Component } from './content1/content1.component';
-
-
+import { AppComponent } from './app.component'
+import { HeaderComponent } from './header/header.component'
+import { FooterComponent } from './footer/footer.component'
+import { Content1Component } from './content1/content1.component'
 
 @NgModule({
   declarations: [
@@ -15,10 +15,8 @@ import { Content1Component } from './content1/content1.component';
     FooterComponent,
     Content1Component
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/content1/content1.component.ts
+++ src/app/content1/content1.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-content1',
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content1.component.css']
 })
 export class Content1Component implements OnInit {
+  @Input() chatMessage: string
+  @Input() chatMessage2: string
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+  public addMessage(message: string): void {
+    // alert(message)
+
+    this.chatMessage = ''
+    this.chatMessage2 =
+      `
+    <div class="chat self">
+          <div class="user-photo"></div>
+            <p class="chat-message">` +
+      message +
+      `</p>
+      </div>`
   }
-
 }
